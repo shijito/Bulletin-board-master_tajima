200753e35359c4d01e80533198aef668c708d8d7
shijito <zero.seiyuu@gmail.com>
2023-06-26 02:41:09 +0900

    カテゴリービュー編集

--- resources/views/auth/authenticated/posts_category.blade.php
+++ resources/views/auth/authenticated/posts_category.blade.php
@@ -2,4 +2,52 @@
 
 @section('content')
 
+<div>
+    <div>
+        <span>カテゴリー追加画面</span>
+    </div>
+    <div>
+        <div>
+            <div>
+                <span>新規メインカテゴリー</span>
+            </div>
+            <div>
+                <!--テキストエリア-->
+                <input type="submit" class="btn btn-primary" value="登録">
+            </div>
+        </div>
+        <div>
+            <div>
+                <div>
+                    <span>メインカテゴリー</span>
+                </div>
+                <div>
+                    <!--セレクト-->
+                </div>
+            </div>
+            <div>
+                <div>
+                    <span>新規サブカテゴリー</span>
+                </div>
+                <div>
+                    <!--テキストエリア-->
+                    <input type="submit" class="btn btn-primary" value="登録">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div>
+            <span>カテゴリー一覧</span>
+        </div>
+        <div><!--foreachでカテゴリーを繰り返す-->
+            <div></div><!--メイン-->
+            <div></div><!--サブ-->
+            <div>
+                <input type="submit" class="btn btn-primary" value="削除">
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
