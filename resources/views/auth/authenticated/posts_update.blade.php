916bd7c3b2a747c6a9ca5752e53ea1bab16f0d8c
shijito <zero.seiyuu@gmail.com>
2023-06-26 02:15:20 +0900

    投稿編集ビュー編集

--- resources/views/auth/authenticated/posts_update.blade.php
+++ resources/views/auth/authenticated/posts_update.blade.php
@@ -2,4 +2,43 @@
 
 @section('content')
 
+<div>
+    <form>
+        <div>
+            <span>投稿編集画面</span>
+        </div>
+        <div>
+            <div>
+                <span>サブカテゴリー</span>
+            </div>
+            <div>
+                <!--セレクト-->
+            </div>
+        </div>
+        <div>
+            <div>
+                <span>タイトル</span>
+            </div>
+            <div>
+                <!--テキストエリア-->
+            </div>
+        </div>
+        <div>
+            <div>
+                <span>投稿内容</span>
+            </div>
+            <div>
+                <!--テキストエリア-->
+            </div>
+        </div>
+        <div>
+            <input type="submit" class="btn btn-primary" value="更新">
+        </div>
+        <div>
+            <input type="submit" class="btn btn-primary" value="削除">
+        </div>
+        {{ csrf_field() }}
+    </form>
+</div>
+
 @endsection
