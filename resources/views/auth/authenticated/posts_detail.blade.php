576993383f13545252f6a5c7875e25f072e46e24
shijito <zero.seiyuu@gmail.com>
2023-06-26 02:25:52 +0900

    詳細ビュー設定

--- resources/views/auth/authenticated/posts_detail.blade.php
+++ resources/views/auth/authenticated/posts_detail.blade.php
@@ -2,4 +2,33 @@
 
 @section('content')
 
+<div>
+    <div>
+        <span>掲示板詳細画面</span>
+    </div>
+    <div>
+        <div>
+            <!--選択した投稿を表示-->
+        </div>
+        <div>
+            <!--カテゴリー-->
+        </div>
+        <div>
+            <span></span><!--コメント数ー-->
+            <span></span><!--いいね数-->
+        </div>
+    </div>
+    <div>
+        <div>
+            <!--コメント投稿-->
+        </div>
+        <div>
+            <!--テキストエリア-->
+        </div>
+        <div>
+            <input type="submit" class="btn btn-primary" value="コメント">
+        </div>
+    </div>
+</div>
+
 @endsection
