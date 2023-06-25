35379fe18231326a242beff115aad7c4f3db4fa3
shijito <zero.seiyuu@gmail.com>
2023-06-26 02:14:42 +0900

    コメントビュー編集

--- resources/views/auth/authenticated/posts_comment.blade.php
+++ resources/views/auth/authenticated/posts_comment.blade.php
@@ -2,4 +2,27 @@
 
 @section('content')
 
+<div>
+    <form>
+        <div>
+            <span>コメント編集画面</span>
+        </div>
+        <div>
+            <div>
+                <span>コメント</span>
+            </div>
+            <div>
+                <!--テキストエリア-->
+            </div>
+        </div>
+        <div>
+            <input type="submit" class="btn btn-primary" value="更新">
+        </div>
+        <div>
+            <input type="submit" class="btn btn-primary" value="削除">
+        </div>
+        {{ csrf_field() }}
+    </form>
+</div>
+
 @endsection
